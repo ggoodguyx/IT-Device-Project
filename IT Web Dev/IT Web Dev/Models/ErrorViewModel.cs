a3de127761d105b70eb5b8592cbddd1d46031fbc
bzwattz <79898653+bzwattz@users.noreply.github.com>
2021-11-19 10:03:18 +0700

    Start

---
+++ IT Web Dev/IT Web Dev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IT_Web_Dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
