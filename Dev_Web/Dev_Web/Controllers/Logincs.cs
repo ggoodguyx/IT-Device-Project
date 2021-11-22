efa7d1c8f14a8b7500acc2ec795fa0c532b63718
ggoodguyx <84179268+ggoodguyx@users.noreply.github.com>
2021-11-22 16:51:07 +0700

    IT-Web
    love u

--- Dev_Web/Dev_Web/Controllers/Logincs.cs
+++ Dev_Web/Dev_Web/Controllers/Logincs.cs
@@ -1,16 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dev_Web.Controllers
 {
-    public class IndexModel : PageModel
+    public class Logincs : Controller
     {
-        public void OnGet()
+        public IActionResult Index()
         {
+            return View();
         }
     }
 }
